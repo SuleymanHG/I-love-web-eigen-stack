54d68d7726475d869aefcdd7a12921c1ed5f5cd5
SuleymanHG <suleyman.gokgul66@outlook.com>
2025-11-08 20:48:07 +0100

    Hero section homepagina #18

---
+++ app/components/HeroSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div>
+        <h1>Hi I'm, Süleyman</h1>
+        <p>I design and build modern <br> web experiences</p>
+    </div>
+    
+    <div>
+        <a href="/cases" class="main-button">View my work</a>
+        <a href="/about" class="secondary-button">About me</a>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+    section{
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        padding: 0px 0 100px 0;
+    }
+
+    div:last-of-type{
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }   
+
+    p{
+        font-size: clamp(1.5rem, 2vw + 1rem, 3rem);
+        margin: 0 0 ;
+    }
+
+    h1{
+        margin:40px 0 5px 0;
+    }
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,3 @@
 <template>
-  <section>
-    <h1>Hi I'm, Süleyman</h1>
-    <p>I design and build modern web experiences</p>
-    
-    <a href="/cases">View my work</a>
-    <a href="/about">About me</a>
-  </section>
+  <<HeroSection />
 </template>
